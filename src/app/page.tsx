e8e9d08ca5d9fb3c7919557aa4d64ba21e217d92
David Pearl <davidtpearl@gmail.com>
2025-05-02 17:11:28 +1000

    Install tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -50,6 +50,9 @@ export default function Home() {
             Read our docs
           </a>
         </div>
+        <h1 className="text-3xl text-green-500 font-bold underline">
+          Hello world!
+        </h1>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
